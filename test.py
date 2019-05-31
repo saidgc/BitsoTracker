689233e3ff37b66c866c09d5ebf5f79b9ffe5454
modulo IV <moduloIV@moduloIV.com>
2019-05-31 12:54:21 -0500

    update

--- test.py
+++ test.py
@@ -4,33 +4,54 @@
 import threading
 import requests
 import json
-
+import numpy as np
 import time
+
+# OPERACION = 0.0001000
+OPERACION = 1000
 
 venta = None
 opera = False
-ganan = 0.0005396
-vactual = 1
+ganan = 1000.0000001
+# ganan = 0.0005396
+vactual = 0
+data = []
+listo = False
 
 def tracking():
-  global venta, opera, ganan, vactual
+  global venta, opera, ganan, vactual, listo
+  
   threading.Timer(0.9, tracking).start()
+  
   response = requests.get('https://api.bitso.com/v3/ticker/?book=btc_mxn')
+  
   if response.status_code != 200:
     raise ApiError('GET /tasks/ {}'. format(response.status_code))
+
   value = float(response.json()["payload"]["last"])
   vwap = float(response.json()["payload"]["vwap"])
-  deal = float(value) * 0.0001
+  deal = value * OPERACION
   percent = (value * 100) / vwap
-  if percent > 100 and not opera:
-    ganan -= 0.0001
+  data.append(value)
+
+  if len(data) > 30:
+    listo = True
+    del data[0]
+
+  if value > np.average(data) and not opera and listo:
+    ganan -= OPERACION
     venta = deal
     opera = True
-    vactual = vwap
-  if ((value * 100) / vactual) < 100 and opera:
-    ganan += deal / value
-    opera = False
-  print(value, vwap, value > vwap, percent, ganan)
-  
+    vactual = np.average(data)
 
-tracking()
+  if value < vactual and opera:
+    ganan += float(deal) / float(value)
+    opera = False
+
+  print(value, vactual, value * ganan, ganan)
+
+
+if __name__ == "__main__":
+  thread = threading.Thread(target=tracking)
+  thread.daemon = False
+  thread.start()
